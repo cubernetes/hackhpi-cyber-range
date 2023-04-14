f53bdc8995478d7bd059b20a96f45e76e19f12b6
cubernetes <timo42@protonmail.com>
2023-04-14 07:57:15 +0200

    Rename

---
+++ blueteam/blueteambot1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+domain=hackhpi23.timo.one
+
+curl -L https://${domain}/api/red -X POST -H "Content-Type: application/json" -d "{\"data\": \"Start of post-exploitation\", \"timestamp\": \"$(date +%s)\"}"
+
+curl -L https://${domain}/api/blue -X POST -H "Content-Type: application/json" -d "{\"data\": \"<b style='color: yellow'>Snort: Intrusion Detected! Blue team should now be aware</b>\", \"timestamp\": \"$(date +%s)\"}"
