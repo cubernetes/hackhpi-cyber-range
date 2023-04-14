23d455aed7be8de903e055f543873344c5883bec
cubernetes <timo42@protonmail.com>
2023-04-14 07:46:52 +0200

    Rename

--- blueteam/auto.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-curl -L https://hackhpi.kyudev.xyz/api/red -X POST -H "Content-Type: application/json" -d "{\"data\": \"Start of post-exploitation\", \"timestamp\": \"$(date +%s)\"}"
-curl -L https://hackhpi.kyudev.xyz/api/blue -X POST -H "Content-Type: application/json" -d "{\"data\": \"\", \"timestamp\": \"$(date +%s)\"}"
